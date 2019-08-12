4022da6c44716254acb150c38e37fb72315c28ed
Jaid <jaid.jsx@gmail.com>
2019-08-12 14:18:09 +0200

    feat: Migrated to `jaid-core` 3
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- src/plugins/main/index.js
+++ src/plugins/main/index.js
@@ -1,19 +1,19 @@
-import Product from "src/models/Product"
-
-export default class {
+export default class Main {
 
   /**
    * @param {import("jaid-core").default} core
    */
-  async init(core) {
+  async init() {
   }
 
-  addModels(addModel) {
+  collectModels() {
+    const models = {}
     const modelsRequire = require.context("../../models/", true, /.js$/)
     for (const value of modelsRequire.keys()) {
       const modelName = value.match(/\.\/(?<key>[\da-z]+)\./i).groups.key
-      addModel(modelName, modelsRequire(value))
+      models[modelName] = modelsRequire(value)
     }
+    return models
   }
 
 }
